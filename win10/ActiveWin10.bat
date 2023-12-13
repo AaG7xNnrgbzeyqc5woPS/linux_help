c4b7eddba2e0a7940258b92bb09a611b9e9a14c8
AaG7xNnrgbzeyqc5woPS <47978362+AaG7xNnrgbzeyqc5woPS@users.noreply.github.com>
2023-12-13 12:24:00 +0800

    Update ActiveWin10.bat

--- win10/ActiveWin10.bat
+++ win10/ActiveWin10.bat
@@ -5,6 +5,8 @@
 # Operating system edition 	KMS Client Product Key
 # Windows 10 Enterprise LTSC 2021
 # Windows 10 Enterprise LTSC 2019 	M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+# Windows 10 Pro for Workstations 	NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
+# Windows 10 Pro 	                  W269N-WFGWX-YVC9B-4J6C9-T83GX
 
 # 右键开始菜单，点击 [命令提示符 (管理员)] 或 [Windows PowerShell (管理员)]
 执行下面的命令：
@@ -15,7 +17,7 @@
 
 slmgr /upk
 slmgr /skms 10.153.70.238  
-slmgr /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+slmgr /ipk NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
 slmgr /ato
 
 slmgr -dlv
