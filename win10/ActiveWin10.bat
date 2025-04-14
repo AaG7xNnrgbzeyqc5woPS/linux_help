94a079ee058ea61298d32b34f4a422e49c14b4f1
bolt_game <john_help@protonmail.com>
2025-04-14 11:23:51 +0800

    Update ActiveWin10.bat

--- win10/ActiveWin10.bat
+++ win10/ActiveWin10.bat
@@ -2,7 +2,7 @@
 文件名：ActiveWin10.bat
 
 # Key Management Services (KMS) client activation and product keys
-# Windows 10/11 Enterprise LTSC 2019/2021/2024 	        M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+# Windows 10/11 Enterprise LTSC 2019/2021/2024 	          M7XTQ-FN8P6-TTKYV-9D4CC-J462D
 # Windows 10/11 Pro for Workstations 	                  NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
 # Windows 10/11 Pro 	                                  W269N-WFGWX-YVC9B-4J6C9-T83GX
 
