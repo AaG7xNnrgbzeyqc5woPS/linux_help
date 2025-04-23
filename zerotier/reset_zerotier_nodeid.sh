12924fc34521e00f282ad5a1d9ee1c3b3336619f
bolt_game <john_help@protonmail.com>
2025-04-23 11:04:34 +0800

    Update reset_zerotier_nodeid.sh

--- zerotier/reset_zerotier_nodeid.sh
+++ zerotier/reset_zerotier_nodeid.sh
@@ -1,5 +1,10 @@
 # filename: reset_zerotier_nodeid.sh
 # reset nodeid(address), mac, ip
+# 更改 ZeroTier 节点号（address）和 MAC 地址
+
+# for linux ubuntu 22.04 lts
+# 2025-4-23
+
 #systemctl status zerotier-one.service
 #systemctl stop zerotier-one.service
 #systemctl start zerotier-one.service
