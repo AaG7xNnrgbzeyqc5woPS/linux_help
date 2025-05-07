3f4942ca7ff6a1a661646e595ec97f92ba9e3109
bolt_game <john_help@protonmail.com>
2025-05-07 13:27:23 +0800

    Update ActiveWin10_11.bat

--- win10/ActiveWin10_11.bat
+++ win10/ActiveWin10_11.bat
@@ -1,11 +1,14 @@
 @goto :start
 文件名：ActiveWin10.bat
 
+#---------------------------------------------------------------------------------------
 # Key Management Services (KMS) client activation and product keys
 # Windows 10/11 Enterprise LTSC 2019/2021/2024 	          M7XTQ-FN8P6-TTKYV-9D4CC-J462D
 # Windows 10/11 Pro for Workstations 	                  NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
 # Windows 10/11 Pro 	                                  W269N-WFGWX-YVC9B-4J6C9-T83GX
-
+# 更多密钥见微软官网：
+# https://learn.microsoft.com/en-us/windows-server/get-started/kms-client-activation-keys?tabs=server2025%2Cwindows1110ltsc%2Cversion1803%2Cwindows81
+#----------------------------------------------------------------------------------------
 # 右键开始菜单，点击 [命令提示符 (管理员)] 或 [Windows PowerShell (管理员)]
 执行下面的命令：
 .\ActiveWin10.bat
